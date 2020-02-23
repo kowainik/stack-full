64b1ee52cb10b204542072b626a63bbdcdbc43d3
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-02-23 17:04:45 +0000

    Create the project

---
+++ src/StackFull.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Kowainik
+SPDX-License-Identifier: BSD-3-Clause
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+See README for more info
+-}
+
+module StackFull
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
